9e57d76d1b9b8c7204b602e67e8a96e68950510c
michael-franke <the.secret.goldfish@me.com>
2024-04-09 17:39:34 +0200

    main functionality for pilot 7

--- association-pilots/pilot7/src/App.vue
+++ association-pilots/pilot7/src/App.vue
@@ -1,10 +1,103 @@
 <template>
   <Experiment title="causal-implicature-variation">
     <InstructionScreen :title="'Welcome'">
-      This is a short short survey where you will read a piece of information and answer a single question about it.
-
-      The task may seem very easy at first, but we ask you to <strong>think about the answer for a moment before you make a decision</strong>.
+      This short survey consists of three tasks.<br>
+      Each part will be introduced in turn.
     </InstructionScreen>
+
+    <InstructionScreen :title="'Task 1'">
+      Your next task is to type five different descriptions to communicate the same idea.
+    </InstructionScreen>
+
+    <template v-for="(trial, i) of trials_repeat">
+      <Screen :key="i">
+
+        <!-- trial 1 -->
+        <Slide>
+          <p>
+            Produce a description to express that <strong>{{ name1 }} causes {{ name2 }}</strong>. (Trial {{i+1}} of 5.)
+            <br>
+              <strong>Your sentence must start with "{{name1}}"!</strong>
+            <br>
+              <span style="color: gray">Example: '{{ name1 }} brings about {{ name2 }}.'</span>
+            <br>
+              <span style="color: gray">Feel free to abbreviate like this: '{{ name1.charAt(0) }} brings about {{ name2.charAt(0) }}.'</span>
+          </p>
+          <TextareaInput
+            :response.sync= "$magpie.measurements.response"
+          />
+          <p v-if= "$magpie.measurements.response && $magpie.measurements.response.length >= 0">
+          <button @click="$magpie.saveAndNextScreen();">Submit</button>
+          </p>
+          <Record
+            :data="{
+              trialNR        : i+1,
+              trialType      : 'generation-subject-is-cause',
+              name1          : trial.name1,
+              name2          : trial.name2,
+              name3          : trial.name3,
+              name4          : trial.name4,
+              name5          : trial.name5,
+              name6          : trial.name6,
+              prompt         : '-',
+              choice1        : trial.choice1,
+              choice2        : trial.choice2,
+              response       : trial.C
+            }"
+          />
+        </Slide>
+      </Screen>
+     </template>
+
+    <InstructionScreen :title="'Task 2'">
+      The next task is almost the same as the last one, but this time you should start with the effect, rather than the cause in your descriptions.
+    </InstructionScreen>
+
+    <template v-for="(trial, i) of trials_repeat">
+      <Screen :key="i">
+
+        <!-- trial 1 -->
+        <Slide>
+          <p>
+            Produce a description to express that <strong>{{ name3 }} causes {{ name4 }}</strong>. (Trial {{i+1}} of 5.)
+            <br>
+              <strong>Your sentence must start with "{{name4}}"!</strong>
+            <br>
+              <span style="color: gray">Example: '{{ name4 }} follows from {{ name3 }}.'</span>
+            <br>
+              <span style="color: gray">Feel free to abbreviate like this: '{{ name4.charAt(0) }} follows from {{ name3.charAt(0) }}.'</span>
+          </p>
+          <TextareaInput
+            :response.sync= "$magpie.measurements.response"
+          />
+          <p v-if= "$magpie.measurements.response && $magpie.measurements.response.length >= 0">
+          <button @click="$magpie.saveAndNextScreen();">Submit</button>
+          </p>
+          <Record
+            :data="{
+              trialNR        : i+1,
+              trialType      : 'generation-subject-is-effect',
+              name1          : trial.name1,
+              name2          : trial.name2,
+              name3          : trial.name3,
+              name4          : trial.name4,
+              name5          : trial.name5,
+              name6          : trial.name6,
+              prompt         : '-',
+              choice1        : trial.choice1,
+              choice2        : trial.choice2,
+              response       : trial.C
+            }"
+          />
+        </Slide>
+      </Screen>
+     </template>
+
+    <InstructionScreen :title="'Task 3'">
+      In the last task, you are given a description similar to the ones you just produced.
+      Your task is to judge which of two interpretations is more likely.
+    </InstructionScreen>
+
 
     <template v-for="(trial, i) of trials">
       <Screen :key="i">
@@ -25,11 +118,14 @@
           <Record
             :data="{
               trialNR        : i,
-              condition      : trial.condition,
+              trialType      : 'FC-interpretation',
               name1          : trial.name1,
               name2          : trial.name2,
+              name3          : trial.name3,
+              name4          : trial.name4,
+              name5          : trial.name5,
+              name6          : trial.name6,
               prompt         : trial.prompt,
-              choiceNameFirst: trial.choiceNameFirst,
               choice1        : trial.choice1,
               choice2        : trial.choice2,
               response       : trial.C
@@ -50,12 +146,24 @@
 import trials from '../trials/trials.csv';
 import _ from 'lodash';
 
+const trials_selected = _.shuffle(trials).slice(-1);
+
 console.log("association-pilots/pilot1")
 
 export default {
   name: 'App',
   data() {
-    return { trials: _.shuffle(trials).slice(-1)};
+    return {
+      name1: trials[0].name1,
+      name2: trials[0].name2,
+      name3: trials[0].name3,
+      name4: trials[0].name4,
+      name5: trials[0].name5,
+      name6: trials[0].name6,
+      trials: trials_selected,
+      // five repetitions of 'trials'
+      trials_repeat: trials_selected.concat(trials_selected).concat(trials_selected),
+    };
   },
   computed: {
     // Expose lodash to template code
