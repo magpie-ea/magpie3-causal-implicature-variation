c61e0b3b11b62f6255e33bf313e79dc00a2a8d97
michael-franke <the.secret.goldfish@me.com>
2024-04-10 15:52:44 +0200

    added data and analyses pilot 7

--- data-and-anlysis/02-data-preparation.R
+++ data-and-anlysis/02-data-preparation.R
@@ -1,4 +1,24 @@
 library(tidyverse)
+library(tidyboot)
+
+## bootstrapped CI
+
+bootstrap_ci <- function(data, R=1000, ci=0.95) {
+  n <- length(data)
+  boot_means <- numeric(R)
+  
+  for(i in 1:R) {
+    sample_indices <- sample(x=1:n, size=n, replace=TRUE)
+    boot_sample <- data[sample_indices]
+    boot_means[i] <- mean(boot_sample)
+  }
+  
+  alpha <- (1 - ci) / 2
+  lower_bound <- quantile(boot_means, probs=alpha)
+  upper_bound <- quantile(boot_means, probs=1-alpha)
+  
+  return(data.frame("lower" = lower_bound, "mean" = mean(data), "upper" = upper_bound))
+}
 
 
 ## Experiment 1
@@ -83,3 +103,69 @@ sum_stats_06 <- data_raw_06 |>
   group_by(condition) |>
   summarize(first_is_cause = mean(response_type == "first-causes-second")) |> 
   mutate(experiment = "Exp. 6")
+
+## Experiment 7
+
+data_raw_07 <- read_csv("01-data-raw/data-raw-pilot-07.csv") |> 
+  mutate(
+    response_type = ifelse(trialType == "FC-interpretation", 
+                           ifelse(str_extract(response, "^[a-zA-Z]+") == str_extract(prompt, "^[a-zA-Z]+"),
+                           "first-causes-second", "second-causes-first"),
+                           "free generation"
+    )) |> 
+  mutate(
+    generation = case_when(
+        group == 'cause-first' & trialType == 'generation-task-1' ~ 'cause-first',
+        group == 'effect-first' & trialType == 'generation-task-2' ~ 'cause-first',
+        group == 'cause-first' & trialType == 'generation-task-2' ~ 'effect-first',
+        group == 'effect-first' & trialType == 'generation-task-1' ~ 'effect-first',
+        TRUE ~ "NA"
+  ))
+
+bad_IDs <- c(3237, 3242, 3238, 3243, 3245, 3279, 3269, 3282, 3287, 3294, 3324, 3329)
+
+sum_stats_07 <- data_raw_07 |>
+  filter(trialType == "FC-interpretation") |>
+  group_by(group) |> 
+  summarize(first_is_cause = mean(response_type == "first-causes-second")) |> 
+  mutate(experiment = "Exp. 7") |> 
+  rename(condition = group)
+
+data_raw_07 |> 
+  filter(trialType != "FC-interpretation") |> 
+  group_by(generation, trialType) |>  
+  reframe(bootstrap_ci(responseTime)) |> 
+  ggplot(aes(x = trialType, y = mean, color = generation, group = generation)) + 
+  geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2) +
+  geom_line() +
+  geom_point(size = 2.5)
+
+data_raw_07 |> 
+  filter(trialType != "FC-interpretation") |> 
+  filter(! submission_id %in% )
+  group_by(generation) |>  
+  reframe(bootstrap_ci(responseTime)) |> 
+  ggplot(aes(x = generation, y = mean, color = generation, group = generation)) + 
+  geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2) +
+  geom_point(size = 2.5)
+
+data_raw_07 |> 
+  filter(trialType != "FC-interpretation") |> 
+  select(submission_id, trialNR, generation, response) |> 
+  pivot_wider(names_from = generation, values_from = response) |> 
+  write_csv('~/Desktop/productions_pilot-07.csv')
+
+## merging all results togeter
+
+sum_stats <- 
+  bind_rows(
+    sum_stats_01, sum_stats_02, sum_stats_03, 
+    sum_stats_04, sum_stats_05, sum_stats_06, 
+    sum_stats_07
+    )
+
+
+
+
+
+
